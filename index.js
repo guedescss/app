fa39dd8126b05476c4eb6d703a16f707dc96e3f6
João Vitor Guedes <138164577+guedescss@users.noreply.github.com>
2024-09-12 10:19:34 -0300

    fundamentos 003

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-// arrays, objetos
+// 1 meta
 let meta = {
     value: 'ler um livro por mês', 
-    adress: 2,
-    checked: false,
+    checked: true,
 }
 
+// varias metas
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos todos os dias", 
+        checked: false,
+    }
+]
 
-//objeto.metodo/função
-meta.value = 777
-console.log(meta.value)
+console.log(metas[1].value);
 
-// function // arrow function
-// const criarMeta = () => {}
