91e42a8051990a79b187115bf9af11f9b785e7b5
João Vitor Guedes <138164577+guedescss@users.noreply.github.com>
2024-09-30 16:21:20 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -61,9 +61,26 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: 'Metas Realizadas',
+        message: 'Metas Realizadas' + realizadas.length,
         choices: [...realizadas]
 
+    })
+}
+
+//água () - caminha () - cantar (x)
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true 
+    })
+
+    if(abertas.length == 0) {
+        console.log('Não existem metas abertas! :)')
+        return
+    }
+
+    await select({
+        message: "Metas Abertas" + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -87,6 +104,10 @@ const start = async () => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -104,6 +125,9 @@ const start = async () => {
             case "realizadas":
                 await metasRealizadas()
                 break
+            case "abertas":
+                await metasAbertas()
+                break                
             case "sair":
                 console.log('Até a próxima!')
                 return
